41c8a7672507e3ed466e399a6f3a1b7cf0a71979
vpaliy <vpaliy97@gmail.com>
2017-05-19 17:53:15 -0400

    data layer : : update repository + add cache function

--- app/src/main/java/com/vpaliy/bakingapp/data/RecipeRepository.java
+++ app/src/main/java/com/vpaliy/bakingapp/data/RecipeRepository.java
@@ -8,10 +8,18 @@ import com.vpaliy.bakingapp.domain.model.Recipe;
 import com.vpaliy.bakingapp.utils.LogUtils;
 
 import rx.Observable;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.annotation.NonNull;
+import android.util.SparseArray;
+
 import javax.inject.Inject;
 
 public class RecipeRepository implements IRepository<Recipe> {
@@ -20,6 +28,8 @@ public class RecipeRepository implements IRepository<Recipe> {
     private final DataSource<RecipeEntity> localDataSource;
     private final DataSource<RecipeEntity> remoteDataSource;
     private final Context context;
+
+    private SparseArray<Recipe> inMemoryCache;
 
     @Inject
     public RecipeRepository(@NonNull DataSource<RecipeEntity> localDataSource,
@@ -30,30 +40,46 @@ public class RecipeRepository implements IRepository<Recipe> {
         this.remoteDataSource=remoteDataSource;
         this.mapper=mapper;
         this.context=context;
+        this.inMemoryCache=new SparseArray<>();
     }
 
     @Override
     public Observable<List<Recipe>> getRecipes() {
         if(isNetworkConnection()){
+            if(inMemoryCache.size()>100) inMemoryCache.clear();
             return remoteDataSource.getRecipes()
                     .doOnNext(this::saveToDisk)
                     .map(mapper::map)
+                    .doOnNext(recipes -> Observable.from(recipes)
+                            .filter(recipe->inMemoryCache.get(recipe.getId())!=null)
+                            .map(recipe -> {
+                                inMemoryCache.put(recipe.getId(),recipe);
+                                return recipe;
+                            }))
                     .doOnNext(list-> LogUtils.logD(list,this));
         }
+        //TODO query the local database
         return null;
     }
 
     private void saveToDisk(List<RecipeEntity> list){
-
+        //TODO save data to the database
     }
 
     @Override
-    public Observable<Recipe> getRecipeById(int recipeId) {
+    public Observable<Recipe> getRecipeById(int id) {
+        if(inMemoryCache.get(id)!=null) {
+            return Observable.just(inMemoryCache.get(id));
+        }
+        //TODO query the database
         return null;
     }
 
 
     private boolean isNetworkConnection(){
-        return false;
+        ConnectivityManager manager=ConnectivityManager.class
+                .cast(context.getSystemService(Context.CONNECTIVITY_SERVICE));
+        NetworkInfo activeNetwork = manager.getActiveNetworkInfo();
+        return activeNetwork!=null && activeNetwork.isConnectedOrConnecting();
     }
 }
