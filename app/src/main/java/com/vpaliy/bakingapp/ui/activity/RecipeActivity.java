a73b0cd5709bbeb8a514584f0a0525a1d0ca6601
vpaliy <vpaliy97@gmail.com>
2017-05-29 23:50:33 -0400

    presentation layer : : set layout for adapter

--- app/src/main/java/com/vpaliy/bakingapp/ui/activity/RecipeActivity.java
+++ app/src/main/java/com/vpaliy/bakingapp/ui/activity/RecipeActivity.java
@@ -3,6 +3,7 @@ package com.vpaliy.bakingapp.ui.activity;
 import android.os.Bundle;
 import com.vpaliy.bakingapp.BakingApp;
 import com.vpaliy.bakingapp.R;
+import com.vpaliy.bakingapp.ui.fragment.RecipesFragment;
 
 public class RecipeActivity extends BaseActivity {
 
@@ -10,8 +11,14 @@ public class RecipeActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setUI();
     }
 
+    private void setUI(){
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.frame,new RecipesFragment())
+                .commit();
+    }
     @Override
     void initializeDependencies() {
         BakingApp.appInstance()
--- app/src/main/java/com/vpaliy/bakingapp/ui/adapter/RecipesAdapter.java
+++ app/src/main/java/com/vpaliy/bakingapp/ui/adapter/RecipesAdapter.java
@@ -2,24 +2,37 @@ package com.vpaliy.bakingapp.ui.adapter;
 
 
 import android.content.Context;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.util.SparseIntArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.github.lzyzsd.randomcolor.RandomColor;
+import com.vpaliy.bakingapp.R;
 import com.vpaliy.bakingapp.domain.model.Recipe;
 import java.util.ArrayList;
 import java.util.List;
+import android.widget.ImageView;
+import android.widget.TextView;
+import butterknife.ButterKnife;
 import android.support.annotation.NonNull;
+import butterknife.BindView;
 
 public class RecipesAdapter extends
         RecyclerView.Adapter<RecipesAdapter.RecipeViewHolder> {
 
     private LayoutInflater inflater;
     private List<Recipe> data;
+    private SparseIntArray colorMap;
+    private final RandomColor randomColor;
 
     public RecipesAdapter(@NonNull Context context){
         this.inflater=LayoutInflater.from(context);
         this.data=new ArrayList<>();
+        this.colorMap=new SparseIntArray();
+        this.randomColor=new RandomColor();
     }
 
     public void setData(List<Recipe> data) {
@@ -31,16 +44,42 @@ public class RecipesAdapter extends
 
     public class RecipeViewHolder extends RecyclerView.ViewHolder {
 
+        @BindView(R.id.recipe_card)
+        CardView recipeCard;
+
+        @BindView(R.id.recipe_title)
+        TextView recipeTitle;
+
+        @BindView(R.id.recipe_image)
+        ImageView recipeImage;
+
         public RecipeViewHolder(View itemView){
             super(itemView);
+            ButterKnife.bind(this,itemView);
         }
 
-        void bindData(){}
+        void applyColor(){
+            int color=colorMap.get(getAdapterPosition());
+            if(color==-1){
+                color=randomColor.randomColor();
+                colorMap.put(getAdapterPosition(),color);
+            }
+            recipeCard.setBackgroundColor(color);
+        }
+        void bindData(){
+            applyColor();
+            Recipe recipe=at(getAdapterPosition());
+            recipeTitle.setText(recipe.getName());
+        }
+    }
+
+    private Recipe at(int index){
+        return data.get(index);
     }
 
     @Override
     public RecipeViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        return new RecipeViewHolder(inflater.inflate(R.layout.adapter_recipe_item,parent,false));
     }
 
     @Override
