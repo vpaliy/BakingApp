efdb4a862e08fae1bef4b328525b4e6e0dcd8b93
vpaliy <vpaliy97@gmail.com>
2017-06-02 10:02:57 -0400

    presentation layer : : add a recipe wrapper

--- app/src/main/java/com/vpaliy/bakingapp/utils/Constants.java
+++ app/src/main/java/com/vpaliy/bakingapp/utils/Constants.java
@@ -6,4 +6,6 @@ public final class Constants {
     public static final String STEPS_TAG="steps_tag";
     public static final String SUMMARY_TAG="summary_tag";
     public static final String SESSION_TAG="media_session_log_tag";
+
+    public static final String EXTRA_WIDGET_DATA="extra_widget_data";
 }
--- app/src/main/java/com/vpaliy/bakingapp/widget/RecipeWidgetService.java
+++ app/src/main/java/com/vpaliy/bakingapp/widget/RecipeWidgetService.java
@@ -1,54 +1,67 @@
 package com.vpaliy.bakingapp.widget;
 
+import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
+import com.vpaliy.bakingapp.R;
+import com.vpaliy.bakingapp.domain.model.Recipe;
+import com.vpaliy.bakingapp.utils.Constants;
 
+import java.util.List;
 
 public class RecipeWidgetService extends RemoteViewsService {
 
     @Override
     public RemoteViewsFactory onGetViewFactory(Intent intent) {
-        return null;
+        RecipeWrapper wrapper=intent.getParcelableExtra(Constants.EXTRA_WIDGET_DATA);
+        return new RecipeRemoteFactory(getApplicationContext(),wrapper.getRecipes());
     }
 
 
-    private class RecipeViewsFactrory implements RemoteViewsService.RemoteViewsFactory {
+    private class RecipeRemoteFactory implements RemoteViewsService.RemoteViewsFactory {
 
-        @Override
-        public void onCreate() {
+        private Context context;
+        private List<Recipe> recipes;
 
+        public RecipeRemoteFactory(@NonNull Context context,
+                                   @Nullable List<Recipe> recipes){
+            this.context=context;
+            this.recipes=recipes;
         }
 
         @Override
-        public int getCount() {
-            return 0;
-        }
+        public void onCreate() {}
+
 
         @Override
-        public void onDataSetChanged() {
-
-        }
+        public void onDataSetChanged() {}
 
         @Override
         public int getViewTypeCount() {
-            return 0;
+            return 1;
         }
 
         @Override
         public RemoteViews getViewAt(int position) {
+            RemoteViews remoteViews=new RemoteViews(context.getPackageName(), R.layout.recipe_widget);
             return null;
         }
 
+        @Override
+        public int getCount() {
+            return recipes!=null?recipes.size():0;
+        }
 
         @Override
         public long getItemId(int position) {
-            return 0;
+            return 1;
         }
 
         @Override
         public void onDestroy() {
-
         }
 
         @Override
